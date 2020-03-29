69bd35c86616ca8334563bd2d5eac8ba2f85330b
HydroGene <32680237+HyydroGene@users.noreply.github.com>
2020-03-29 10:25:33 +0400

    Add files via upload

--- app/src/main/java/com/example/tp_scurit_info/Cesar.java
+++ app/src/main/java/com/example/tp_scurit_info/Cesar.java
@@ -1,4 +1,4 @@
-package com.example.tp_scurit_info;
+
 public class Cesar {
 
 	public Cesar() {
@@ -13,16 +13,29 @@ public class Cesar {
 			int character = (int)message.charAt(i);
 			int next = ExtendedAscii.getChar(character, offset);
 
-			if (ExtendedAscii.isSpecialCharacter(next)) {
-				
+			if (ExtendedAscii.isSpecialCharacter(next)) 
 				message_final += ExtendedAscii.getHex(character);
-			}
-			
+
 			else
-			{
-				// System.out.println("Next: " + (char)next + " | " + (int)next);
 				message_final += ExtendedAscii.getString(next); //(char)next;
-			}
+			
+		}
+
+		return message_final;
+	}
+	
+	public String decrypt(String message, int offset) {
+		String message_final = "";
+
+		for(int i = 0; i < message.length(); i++)
+		{
+			int character = (int)message.charAt(i);
+			int next = ExtendedAscii.getChar(character, -offset);
+
+			if (ExtendedAscii.isSpecialCharacter(next)) 
+				message_final += ExtendedAscii.getHex(character);
+			else
+				message_final += ExtendedAscii.getString(next); //(char)next;
 		}
 
 		return message_final;
